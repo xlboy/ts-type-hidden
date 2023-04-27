1e45a5d469f6ebbceb17403ad14ea2d5a89d3324
xlboy <xlboy88888@foxmail.com>
2023-04-27 19:05:01 +0800

    feat(vscode.commands): add `ts-type-hidden.(hideType|showType)`

--- src/core/command.ts
+++ src/core/command.ts
@@ -1,12 +1,21 @@
 import vscode from 'vscode';
 
-import { log } from './log';
 import { EditorContext } from './editor-context';
+import { log } from './log';
 
 export function registerCommand(vscodeContext: vscode.ExtensionContext) {
   vscodeContext.subscriptions.push(
     vscode.commands.registerCommand('ts-type-hidden.toggleHiddenMode', () => {
       EditorContext.i.toggleHiddenMode();
+    }),
+    vscode.commands.registerCommand('ts-type-hidden.hideType', () => {
+      log.appendLine(`[command.hideType] Hide type`)
+      EditorContext.i.hideType(true);
+    }),
+
+    vscode.commands.registerCommand('ts-type-hidden.showType', () => {
+      log.appendLine(`[command.showType] Show type`)
+      EditorContext.i.showType();
     })
   );
 }
