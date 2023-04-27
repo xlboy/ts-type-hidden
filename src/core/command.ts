ad4e5cef9879f2ce28d80348ebb8a2134ecec09e
xlboy <xlboy88888@foxmail.com>
2023-04-27 14:26:50 +0800

    feat(vscode.commands): `toggleHiddenType` -> `toggleHiddenMode`

--- src/core/command.ts
+++ src/core/command.ts
@@ -5,9 +5,8 @@ import { EditorContext } from './editor-context';
 
 export function registerCommand(vscodeContext: vscode.ExtensionContext) {
   vscodeContext.subscriptions.push(
-    vscode.commands.registerCommand('ts-type-hidden.toggleHiddenType', () => {
-      log.appendLine('toggleHiddenType');
-      EditorContext.i.toggleHiddenType();
+    vscode.commands.registerCommand('ts-type-hidden.toggleHiddenMode', () => {
+      EditorContext.i.toggleHiddenMode();
     })
   );
 }
